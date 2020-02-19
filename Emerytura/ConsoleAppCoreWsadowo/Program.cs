d8e8668b92bc1cf09bf3257bf4b972229e3ff1f6
Mikołaj Strusiński <mikistrus@gmail.com>
2020-02-19 19:35:33 +0100

    Update Program.cs

--- Emerytura/ConsoleAppCoreWsadowo/Program.cs
+++ Emerytura/ConsoleAppCoreWsadowo/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿//Aplikacja w .NET Core
+//aplikacja dziala w trybie wsadowym, wykorzystuje parametry 
+//przekazane w linii komend
+//przykład użycia: app.exe imie Nazwisko Wiek
+
+
+using System;
 
 namespace ConsoleAppCoreWsadowo
 {
     class Program
     {
+        const int WiekEmerytalny = 67; //stała globalna  
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string imie = args[0];
+            string nazwisko = args[1];
+            int wiek = int.Parse(args[2]);
+
+            Console.WriteLine($"Witaj {imie} {nazwisko}!");
+
+
+
+            Console.WriteLine($"Masz {wiek} lat.");
+            if (wiek >= WiekEmerytalny) Console.WriteLine("Jestes emerytem");
+            else Console.WriteLine($"Do emerytury zostalo ci: {WiekEmerytalny - wiek}");
         }
     }
 }
